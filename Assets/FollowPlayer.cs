2d09ae04fc602cf37bd7ddbd9d5a736e05e89d6b
Yudi <78460589+Yudi1504@users.noreply.github.com>
2022-11-21 22:28:37 +0700

    first commit

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject player;
+
+    [SerializeField]
+    private Vector3 offset;
+
+    private void Start()
+    {
+        transform.position = player.transform.position + offset;
+    }
+
+    private void Update()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private TerrainGenerator terrainGenerator;
+
+    private Animator animator;
+    private bool isHopping;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W) && !isHopping)
+        {
+            float zDifference = 0;
+
+            if (transform.position.z % 1 != 0)
+            {
+                zDifference = Mathf.Round(transform.position.z) - transform.position.z;
+            }
+            MoveCharacter(new Vector3(1, 0, zDifference));
+        }
+        else if (Input.GetKeyDown(KeyCode.A) && !isHopping) //Left
+        {
+            MoveCharacter(new Vector3(0, 0, 1));
+        }
+        else if (Input.GetKeyDown(KeyCode.D) && !isHopping) //Right
+        {
+            MoveCharacter(new Vector3(0, 0, -1));
+        }
+    }
+
+    private void MoveCharacter(Vector3 difference)
+    {
+        animator.SetTrigger("hop");
+        isHopping = true;
+        transform.position = (transform.position + difference);
+        terrainGenerator.SpawnTerrain(false, transform.position);
+    }
+
+    public void FinishHop()
+    {
+        isHopping = false;
+    }
+}
